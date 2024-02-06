38180260ac79ade777450d079101d247f044d334
kokyawlin <kokyawlin@gmail.com>
2024-02-06 22:19:32 +0800

    update api 6-Feb
    update api 6-Feb

--- sgevent-api/src/main/java/com/nus/sgevent/MainController.java
+++ sgevent-api/src/main/java/com/nus/sgevent/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import com.nus.sgevent.entity.*;
 
 
 @Controller	// This means that this class is a Controller
