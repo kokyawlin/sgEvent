b50e598f83a2296259d99d7d70e3311d2d5cb551
kokyawlin <kokyawlin@gmail.com>
2024-03-26 15:53:31 +0800

    removed spring security

--- sgevent-api/src/main/java/com/nus/sgevent/SGEventApplication.java
+++ sgevent-api/src/main/java/com/nus/sgevent/SGEventApplication.java
@@ -3,6 +3,7 @@ package com.nus.sgevent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SGEventApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
