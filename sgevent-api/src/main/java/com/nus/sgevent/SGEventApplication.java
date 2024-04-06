93eb384c4550a2a3d1c3fbe2dceb116b9bd286e1
FangZiyue <e0838388@u.nus.edu>
2024-04-06 15:28:44 +0800

    use parameter to load db&mail password instead of storing in the prop file

--- sgevent-api/src/main/java/com/nus/sgevent/SGEventApplication.java
+++ sgevent-api/src/main/java/com/nus/sgevent/SGEventApplication.java
@@ -1,22 +1,26 @@
 package com.nus.sgevent;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SGEventApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
+
+    if (args.length > 0) {
+      System.setProperty("db.username", args[0]);
+      System.setProperty("db.password", args[1]);
+      System.setProperty("mail.username", args[2]);
+      System.setProperty("mail.username", args[3]);
+    }
+
     SpringApplication.run(SGEventApplication.class, args);
   }
 
